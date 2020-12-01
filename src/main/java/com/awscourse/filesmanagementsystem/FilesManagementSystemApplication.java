87a3d67d2dcbbdc1e269b3bb9ad40c9b096cd329
MatCoder77 <matkimzag@wp.pl>
2020-12-01 18:57:58 +0100

    Initial commit

---
+++ src/main/java/com/awscourse/filesmanagementsystem/FilesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.awscourse.filesmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awscourse/filesmanagementsystem/FilesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awscourse.filesmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
